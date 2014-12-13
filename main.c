480b606979017013ec5815ecf8a729cd2c1eee3e
joshkern <kern.j@husky.neu.edu>
2014-12-13 18:28:41 -0500

    End of day before Costa Rica

--- main.c
+++ main.c
@@ -23,18 +23,23 @@ int main(void) {
 /////// 			Setup Timer A1 for PWM							/////////
 /////////////////////////////////////////////////////////////////////////////
 
-    P2DIR |= BIT0;                       // P2.0 output
-    P2SEL |= BIT0;                       // P2.0 options select
+    P2DIR |= BIT0;                       // Set P2.0 as output
+    P2SEL |= BIT0;                       // Set P2.0 to output 1 of timer A1
     TA1CCR0 = 100-1;                     // PWM Period
     TA1CCTL1 = OUTMOD_7;                 // CCR1 reset/set
     TA1CCR1 = 49;                        // CCR1 PWM duty cycle
-    TA1CTL = TASSEL_2 + MC_1 + TACLR;    // SMCLK, up mode, clear TAR
+    TA1CCR2 = 49;                        // CCR2 PWM duty cycle
+    TA1CCR3 = 49;                        // CCR3 PWM duty cycle
+    TA1CTL = TASSEL_2 + TACLR;    // SMCLK, clear TAR
 
  /////////////////////////////////////////////////////////////////////////////
  /////// 			Setup Timer A0 for LED Selection				 /////////
  /////////////////////////////////////////////////////////////////////////////
 
-    TA0CCR0 =
+    TA0CCR0 = 1000 - 1;					// PWM Period
+    TA1CCTL2 = OUTMOD_7;				// CCR2 reset/set
+    TA0CCR1 = 100 - 1;					// Trigger for ADC
+    TA0CCR2 = 300 - 1;					// PWM Duty Cycle
 
     __bis_SR_register(LPM0_bits);             // Enter LPM0
     __no_operation();                         // For debugger
